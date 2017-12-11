7ea97f78b36a92bc5052f5a00558a69f13c1bfeb
Ciarán Ó hAoláin <c.ohaolain@gmail.com>
2017-12-11 22:26:01 +0000

    Add improvements to Bitcoin Prediction Algorithm

--- Bitcoin Prediction Algorithm/PredictionAlgorithm.py
+++ Bitcoin Prediction Algorithm/PredictionAlgorithm.py
@@ -11,6 +11,9 @@ print("\n\tInput the number of day's predictions you want to make:", end="\n\t")
 while 1:
     try:
         numDaysPredict = int(input())  # How many days worth of prices to predict
+        if numDaysPredict < 1:
+            print("\n\tInvalid entry. Try again:", end='\n\t')
+            continue
         print()
         break
     except ValueError:
@@ -23,7 +26,7 @@ exc = db.cursor()
 # Find last data point
 exc.execute("SELECT timestamp, weighted_price FROM data ORDER BY timestamp DESC LIMIT 1")
 initialValues = exc.fetchone()
-initDate = datetime.datetime.fromtimestamp(initialValues[0]).strftime('%Y-%m-%d')
+initDate = datetime.datetime.fromtimestamp(initialValues[0]).strftime('%Y-%m-%d %H:%M')
 initWeekday = datetime.datetime.fromtimestamp(initialValues[0]).strftime('%a')
 startNote = "\tThe starting value (from " + initWeekday + " " + initDate + ") is $" + str(round(initialValues[1], 2))
 
@@ -39,27 +42,20 @@ exc.execute("SELECT timestamp, weighted_price FROM data WHERE timestamp > ? and 
 # Declare variables to store average growths, both daily (deprecated) and per-weekday, and also the previous day's value
 previous = 0
 count = 0
-# growth = 0
 dayGrowth = [0, 0, 0, 0, 0, 0, 0]
 dayCount = [0, 0, 0, 0, 0, 0, 0]
 
 # Check daily values and store relevant deltas
 for val in exc.fetchmany(numEntries):
-
     if count == 0:
         count += 1
         previous = val[1]
         continue
-
     count += 1
-    # growth += val[1]/previous
     dayNum = int(datetime.datetime.fromtimestamp(val[0]).strftime('%w'))
     dayCount[dayNum] += 1
     dayGrowth[dayNum] += val[1] / previous
     previous = val[1]
-
-# Calculate average daily growth
-# averageGrowth = growth/(count-1)
 
 # Calculate average per-weekday growth
 for i in range(0, len(dayGrowth)):
@@ -69,18 +65,12 @@ for i in range(0, len(dayGrowth)):
 lastDay = initialValues[1]
 print("\n" + startNote + "\n")
 for i in range(numDaysPredict):
-    date = datetime.datetime.fromtimestamp(initialValues[0]+((i+1)*86400)).strftime('%Y-%m-%d')
-
-    # answer = initialValues[1]*averageGrowth**(i+1)
-
-    # Day-by-day answer
+    # Calculate and print day-by-day answers
+    date = datetime.datetime.fromtimestamp(initialValues[0]+((i+1)*86400)).strftime('%Y-%m-%d %H:%M')
     dayNum = int(datetime.datetime.fromtimestamp(initialValues[0] + ((i + 1) * 86400)).strftime('%w'))
     weekday = datetime.datetime.fromtimestamp(initialValues[0] + ((i + 1) * 86400)).strftime('%a')
-
     lastDay = lastDay * dayGrowth[dayNum]
-
-    print("\tDay " + str(i + 1) + ": " + weekday + " " + date + "\t" + str(round(lastDay, 2)))
+    print("\tDay " + str(i + 1) + ":\t" + weekday + " " + date + "\t" + str(round(lastDay, 2)))
 
 print()
-	
 db.close()
